ddbb093a02780983dc18c7803800143e4f675214
vaneybr <vaneybr@gmail.com>
2025-05-28 20:56:54 -0600

    añadi ss a boton reservaciones

--- src/main/java/com/vane/hotel/HelloApplication.java
+++ src/main/java/com/vane/hotel/HelloApplication.java
@@ -31,7 +31,7 @@ public class HelloApplication extends Application {
             });
         }
 
-        Button btnReservaciones = (Button) menuScene.lookup("#btnReservaciones");
+        Button btnReservaciones = (Button) menuScene.lookup("#btnReservacionessss");
         if (btnReservaciones != null) {
             btnReservaciones.setOnAction(event -> {
                 try {
